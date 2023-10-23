f96b5a1250799c6e31b441dd5feacfb22e3d6255
Ishaan Gaba <ishaangaba97@gmail.com>
2023-10-24 01:29:20 +0530

    first commit

---
+++ src/Contact.js
@@ -0,0 +1,88 @@
+import styled from "styled-components";
+
+const Contact = () => {
+  const Wrapper = styled.section`
+    padding: 9rem 0 5rem 0;
+    text-align: center;
+
+    .container {
+      margin-top: 6rem;
+
+      .contact-form {
+        max-width: 50rem;
+        margin: auto;
+
+        .contact-inputs {
+          display: flex;
+          flex-direction: column;
+          gap: 3rem;
+
+          input[type="submit"] {
+            cursor: pointer;
+            transition: all 0.2s;
+
+            &:hover {
+              background-color: ${({ theme }) => theme.colors.white};
+              border: 1px solid ${({ theme }) => theme.colors.btn};
+              color: ${({ theme }) => theme.colors.btn};
+              transform: scale(0.9);
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  return (
+    <Wrapper>
+      <h2 className="common-heading">Contact page</h2>
+
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14003.0762650265!2d77.18042852610758!3d28.666632577096337!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cfd7f67c9890f%3A0xb0955976f8b0f82f!2sPratap%20Nagar!5e0!3m2!1sen!2sin!4v1698086633873!5m2!1sen!2sin" 
+        width="100%"
+        height="400"
+        style={{ border: 0 }}
+        allowFullScreen=""
+        title = "myMap"
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"></iframe>
+
+      <div className="container">
+        <div className="contact-form">
+          <form
+            // action="https://formspree.io/f/xeqdgwnq"
+            method="POST"
+            className="contact-inputs">
+            <input
+              type="text"
+              placeholder="username"
+              name="username"
+              required
+              autoComplete="off"
+            />
+
+            <input
+              type="email"
+              name="Email"
+              placeholder="Email"
+              autoComplete="off"
+              required
+            />
+
+            <textarea
+              name="Message"
+              cols="30"
+              rows="10"
+              required
+              autoComplete="off"
+              placeholder="Enter you message"></textarea>
+
+            <input type="submit" value="send" />
+          </form>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Contact;
